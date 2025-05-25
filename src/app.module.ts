c24582bd343913625fc87286ba8e49708a0b38d8
graff012 <sobirjonovnuriddin43@gmail.com>
2025-05-26 01:20:26 +0500

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { CoreModule } from './core/core.module';
-import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [UsersModule, CoreModule, MoviesModule],
+  imports: [UsersModule, CoreModule],
   controllers: [],
   providers: [],
 })
